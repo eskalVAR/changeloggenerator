c667070ffd346978030b575d8984db267a4c50c9
Eryk Skalinski <e22rico@gmail.com>
2017-10-23 23:41:39 +0100

    Started printDir function

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,22 @@
 using namespace std;
 
 
+void printDir(void) {
+	//http://www.cplusplus.com/forum/beginner/43934/
+	DIR *dp;
+	struct dirent *ep;
+	struct stat stat_output;
+	//http://pubs.opengroup.org/onlinepubs/009695399/functions/stat.html
+	//Convert to char array as opendir requires const char  * since it's a C routine
+	//http://pubs.opengroup.org/onlinepubs/7908799/xsh/dirent.h.html
+	if ((dp = opendir(dir.c_str())) == NULL)
+	{
+		return "ERROR";
+	};
+
+}
+
+
 int main()
 {
     return 0;
