028b5a2d7e1dccf3a3764659a53be139f1d6602b
Eryk Skalinski <e22rico@gmail.com>
2017-10-23 23:37:33 +0100

    Boiler Plate

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
+//Changelog Generator 2.0 By Eryk Skalinski 19/10/2017 (DD/MM/YYYY)
+
+//Standard Includes
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+//Directory Managment Imports
+#include<dirent.h>
+#include<sys/stat.h>
+#include <sys/types.h>
+//https://stackoverflow.com/questions/5530933/dirent-h-in-visual-studio-2010-or-2008
 
-int main() {
+using namespace std;
 
+
+int main()
+{
+    return 0;
 }
+
